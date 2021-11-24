d8baa2dde2edf3117d748a8dd69ef1a34aafff8e
asif-iqbal-munna <asifiqbalmunna1318@gmail.com>
2021-11-25 02:58:27 +0600

    user role upgradation to admin functionality completed

--- src/pages/Dashboard/MakeAdmin.js/MakeAdmin.js
+++ src/pages/Dashboard/MakeAdmin.js/MakeAdmin.js
@@ -1,11 +1,26 @@
 import { Button, Container, TextField } from "@mui/material";
 import { Box } from "@mui/system";
+import axios from "axios";
 import React from "react";
 import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
 const MakeAdmin = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = (data) => {
+    const user = { email: data.email };
+    axios.put("https://tranquil-cove-40150.herokuapp.com/users/admin", user).then((res) => {
+      if (res.data?.modifiedCount) {
+        Swal.fire({
+          icon: "success",
+          title: "Bravo!",
+          text: `${data.email} Has Been Given The Admin Role`,
+          confirmButtonText: "Cheers",
+        });
+      }
+      reset();
+    });
+  };
 
   return (
     <Container sx={{ my: 10 }}>
