66ea13b390bf763d7214a0a8215cba3e35e7b316
asif-iqbal-munna <asifiqbalmunna1318@gmail.com>
2021-11-25 13:14:49 +0600

    booking deletation functionality is completed with confirmation

--- src/pages/Dashboard/Bookings/Bookings.js
+++ src/pages/Dashboard/Bookings/Bookings.js
@@ -1,69 +1,37 @@
-import {
-  TableBody,
-  Table,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Button,
-} from "@mui/material";
+import { Typography, Grid } from "@mui/material";
 import { Box } from "@mui/system";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import useAuth from "../../../hooks/useAuth";
+import SingleBooking from "./SingleBooking";
 
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
+  const [fetch, setFetch] = useState(false);
   const { user } = useAuth();
 
   useEffect(() => {
     axios
       .get(`https://tranquil-cove-40150.herokuapp.com/bookings/${user.email}`)
       .then((res) => setBookings(res.data));
-  }, [user.email]);
+  }, [user.email, fetch]);
 
   return (
     <Box sx={{ px: 2 }}>
-      <TableContainer component={Paper}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>&nbsp;</TableCell>
-              <TableCell>Hotel</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Fat&nbsp;(g)</TableCell>
-              <TableCell>Status</TableCell>
-              <TableCell>Payment</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {bookings.map((row) => (
-              <TableRow
-                key={row._id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell>
-                  <img src={row.hotelData.img} width="100px" alt="" />
-                </TableCell>
-                <TableCell>{row.hotelData.name}</TableCell>
-                <TableCell>{row.email}</TableCell>
-                <TableCell>{row.fat}</TableCell>
-                <TableCell>{row.status}</TableCell>
-                <TableCell>
-                  {row?.paid ? (
-                    ""
-                  ) : (
-                    <Button size="small" variant="contained">
-                      Pay
-                    </Button>
-                  )}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <Typography gutterBottom align="center" variant="h3">
+        Your Bookings
+      </Typography>
+      <Box>
+        <Grid container spacing={4}>
+          {bookings.map((booking) => (
+            <SingleBooking
+              key={booking._id}
+              booking={booking}
+              setFetch={setFetch}
+            />
+          ))}
+        </Grid>
+      </Box>
     </Box>
   );
 };
---
+++ src/pages/Dashboard/Bookings/SingleBooking.js
@@ -0,0 +1,117 @@
+import {
+  Badge,
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  CircularProgress,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import axios from "axios";
+import React from "react";
+import Swal from "sweetalert2";
+
+const SingleBooking = ({ booking, setFetch }) => {
+  const { name, price, img } = booking.hotelData;
+
+  const handleDeleteOrder = (id) => {
+    setFetch(true);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios.delete(`http://localhost:8000/booking/${id}`).then((res) => {
+          if (res.data.deletedCount) {
+            Swal.fire("Deleted!", "Your booking has been deleted.", "success");
+          }
+          setFetch(false);
+        });
+      }
+    });
+  };
+
+  return (
+    <>
+      {!booking ? (
+        <Box
+          sx={{
+            display: "flex",
+            height: "100vh",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Grid item xs={12} md={6}>
+          <Card sx={{ display: "flex" }}>
+            <CardMedia
+              component="img"
+              sx={{ width: 120 }}
+              image={img}
+              alt="Live from space album cover"
+            />
+            <Box
+              sx={{ display: "flex", flexDirection: "column", width: "100%" }}
+            >
+              <CardContent sx={{ flex: "1 0" }}>
+                <Typography component="div" variant="h4">
+                  {name}
+                </Typography>
+                <Badge
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "right",
+                  }}
+                  color="secondary"
+                  badgeContent={booking.status}
+                >
+                  <Typography
+                    sx={{ m: 1 }}
+                    variant="h4"
+                    color="text.secondary"
+                    component="div"
+                  >
+                    ${price}
+                  </Typography>
+                </Badge>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    pt: 2,
+                  }}
+                >
+                  <Button
+                    onClick={() => handleDeleteOrder(booking._id)}
+                    variant="contained"
+                    size="small"
+                    color="primary"
+                  >
+                    Cancel
+                  </Button>
+                  <Box>
+                    <Button variant="contained" color="secondary" size="small">
+                      Pay
+                    </Button>
+                  </Box>
+                </Box>
+              </CardContent>
+            </Box>
+          </Card>
+        </Grid>
+      )}
+    </>
+  );
+};
+
+export default SingleBooking;
